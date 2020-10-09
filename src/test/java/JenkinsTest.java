797ea159bd4110e038965319ed1f895ca39bb6f5
Ernesto231 <max2power222@gmail.com>
2020-10-09 00:46:12 -0400

    JenkinTest add

---
+++ src/test/java/JenkinsTest.java
@@ -0,0 +1,13 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class JenkinsTest {
+
+    @Test
+    public void SampleTest(){
+        int a = 2;
+        int b = 3;
+
+        assertTrue(a+b == 5);
+    }
+}
